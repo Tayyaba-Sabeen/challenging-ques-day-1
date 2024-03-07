144df5be070eaff420b9cc1ec92a640750ebf917
Tayyaba-Sabeen <161269662+Tayyaba-Sabeen@users.noreply.github.com>
2024-03-07 06:04:01 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,8 @@
+let firstName = "tayyaba";
+console.log('hellow', firstName, 'would you like to learn some Typescript today?');
+let personalName = "tayyaba sabeen"
+console.log(personalName.toLowerCase());
+console.log(personalName.toUpperCase());
+console.log(personalName.charAt(0).toUpperCase()+personalName.slice(1).toLowerCase());
+
+console.log('Albert einstien once said,"A person who never made a mistake never tried anything new,"');
